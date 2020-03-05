faf5093376f71e562b3e16aae8492f974e6a2ab0
_______ <53409082+xannyyyy@users.noreply.github.com>
2020-03-05 16:07:18 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('js/particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
